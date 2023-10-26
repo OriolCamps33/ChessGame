15496de415724c8b3df30db1e239878c617a4cbe
1600363 <101714225+1600363@users.noreply.github.com>
2023-10-26 23:19:50 +0200

    Main Marc

--- ChessGame.cpp
+++ ChessGame.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
 #include "Model/m_test.h"
 #include "Model/m_tablero.h"
+#include "Model/m_partida.h"
 using namespace std;
 
 
 int main(int argc, char const* argv[])
 {
-	testAll();
+
+	m_partida partida = m_partida();
+
+	partida.Jugar();
+
+	bool winner = false;
+
+	//testAll();
 }
