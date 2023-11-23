4266775c610c0509daed5b37264438515a698764
1600363 <101714225+1600363@users.noreply.github.com>
2023-11-23 21:12:50 +0100

    Add files via upload

--- ChessGame.cpp
+++ ChessGame.cpp
@@ -7,25 +7,17 @@ using namespace std;
 
 #define color SetConsoleTextAttribute
 
+
 int main(int argc, char const* argv[])
 {
-	/*
-	
-	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-
-	for (int i = 1; i < 243; i++) {
-		color(hConsole, i);
-		cout << i << " - Hola Mundo!!" << endl;
-	}
-	
-
 	m_partida partida = m_partida();
 
-	partida.Jugar();
+	partida.CrearPlayers();
 
-	bool winner = false;
-	*/
+	string ganador = partida.Jugar();
 
-	testJaque();
+	partida.ActualizaPuntuacion(ganador);
+
+	//testAll();
 
 }
