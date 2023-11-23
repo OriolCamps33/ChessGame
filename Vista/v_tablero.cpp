a0e0fb919cc58a17817b4b50fa96043390ee5abc
1600363 <101714225+1600363@users.noreply.github.com>
2023-11-23 21:12:30 +0100

    Add files via upload

--- Vista/v_tablero.cpp
+++ Vista/v_tablero.cpp
@@ -4,14 +4,14 @@
 #include "../Model/m_tablero.h"
 using namespace std;
 
-void m_tablero::printaTablero(bool clean){
+void m_tablero::printaTablero(bool clean) {
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-    
+
     // creamos matriz
     string tablero2[numRow][numCol];
     //inicializamos con string vacios de momento
-    for (int i = 0; i < numRow; i++){
-        for (int j = 0; j < numCol; j++){
+    for (int i = 0; i < numRow; i++) {
+        for (int j = 0; j < numCol; j++) {
             string ico = tablero[i][j]->getIcono();
             if (ico == "-1") {
                 tablero2[i][j] = "";
@@ -22,25 +22,25 @@ void m_tablero::printaTablero(bool clean){
             }
         }
     }
-    
-    if(clean) // limpiamos la pantalla antes de printar nada
+
+    if (clean) // limpiamos la pantalla antes de printar nada
         system("cls");
-//  149 blanco, 128 negro
+    //  149 blanco, 128 negro
 
 
-    //printar tablero
-    for(int i = int(numRow)-1; i >= 0; i--){
+        //printar tablero
+    for (int i = int(numRow) - 1; i >= 0; i--) {
 
         //lineas horizontales
         cout << endl << string(2, ' ');
-        for(int j = 0; j < numCol; j++){
+        for (int j = 0; j < numCol; j++) {
             cout << setfill('-') << setw(5) << left << '+';
         }
         cout << '+' << endl;
 
         //lineas verticales
-        cout << setfill(' ') << setw(2) << left << i+1;
-        for(int j = 0; j<numCol; j++){
+        cout << setfill(' ') << setw(2) << left << i + 1;
+        for (int j = 0; j < numCol; j++) {
             cout << setfill(' ') << setw(2) << left << '|';
             if (tablero[i][j]->getColor() == 1) {
                 // set del color blanco
@@ -48,7 +48,7 @@ void m_tablero::printaTablero(bool clean){
             }
             else {
                 // set del color negro
-                SetConsoleTextAttribute(hConsole,9);
+                SetConsoleTextAttribute(hConsole, 9);
             }
             cout << setfill(' ') << setw(3) << left << tablero2[i][j];
             SetConsoleTextAttribute(hConsole, 7);
@@ -56,8 +56,8 @@ void m_tablero::printaTablero(bool clean){
         cout << '|';
     }
 
-    cout << endl << string(2,' ');
-    for (int i = 0; i < numCol; i++){
+    cout << endl << string(2, ' ');
+    for (int i = 0; i < numCol; i++) {
         cout << setfill('-') << setw(5) << left << 'x';
     }
     cout << '+';
@@ -66,7 +66,7 @@ void m_tablero::printaTablero(bool clean){
     cout << endl;
     cout << string(5, ' ');
     for (int i = 0; i < numCol; i++) {
-        cout << setfill(' ') << setw(5) << left << char('A'+i);
+        cout << setfill(' ') << setw(5) << left << char('A' + i);
     }
     cout << ' ' << endl;
 }
